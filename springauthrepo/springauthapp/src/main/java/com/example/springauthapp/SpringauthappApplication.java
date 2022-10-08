117ffff0d8387caecfcfa4d0907cf9b06a7d60f9
rinadkr <115210475+rinadkr@users.noreply.github.com>
2022-10-08 09:48:02 +0530

    Add files via upload

---
+++ springauthrepo/springauthapp/src/main/java/com/example/springauthapp/SpringauthappApplication.java
@@ -0,0 +1,13 @@
+package com.example.springauthapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringauthappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringauthappApplication.class, args);
+	}
+
+}
---
+++ springauthrepo/springauthapp/src/test/java/com/example/springauthapp/SpringauthappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springauthapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringauthappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
